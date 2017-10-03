f70be28df9ba10ed8533d10bbc7267b908df49aa
Ben Blackman <bblkmn5@gmail.com>
2017-10-03 01:44:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+  array = [0..max]
+  array[0] = array[1] = nil
+
+    i = 0
+  array.each do |p|
+    if p * p > max
+      break
+    end
+    counter+=1
+    (p*p).step(max,p) { |m| array[m] = nil}
+  end
+    
+  
+end
