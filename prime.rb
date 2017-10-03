c570be2544171790f9e1283e78973d7b68529d0a
Ben Blackman <bblkmn5@gmail.com>
2017-10-03 01:47:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,12 @@
 def prime?(integer)
   array = (0..integer).to_a
 #  array[0] = array[1] = nil
-
-    i = 0
+  i = 0
   array.each do |p|
     if (p*p) > integer
       break
     end
-    counter+=1
+    i+=1
     (p*p).step(integer,p) { |m| array[m] = nil}
   end
 
