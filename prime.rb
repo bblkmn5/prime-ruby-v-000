b8b56fdecb4a309c8f6e410da708b709e0e41f63
Ben Blackman <bblkmn5@gmail.com>
2017-10-03 02:12:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(num)
   num.abs
   array = (0...num).to_a
-  array [0] = array[1] = nil
+  next if num === 0
   return false if num < 2
-  return nil if num === 0
   number = array.find { |x| num % x == 0}
   !number
 end
